e6b238fb4d8eaca11e236aa522dcb620ff1e3766
Roy Fernandez <u20221b778@upc.edu.pe>
2025-06-21 20:09:42 -0500

    feat: Add JWT generation service

--- Greenlogist.Api/Program.cs
+++ Greenlogist.Api/Program.cs
@@ -2,6 +2,7 @@ using Greenlogist.Application.Interfaces;
 using Greenlogist.Application.Services;
 using Greenlogist.Domain;
 using Greenlogist.Infrastructure;
+using Greenlogist.Infrastructure.Authentication; // <-- AÑADIR ESTE USING
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,9 +19,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-// 3. Registrar nuestros servicios de aplicación (¡NUEVA LÍNEA!)
+// 3. Registrar nuestros servicios de aplicación
 builder.Services.AddScoped<IAuthService, AuthService>();
-
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>(); // <-- AÑADIR ESTA LÍNEA
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
